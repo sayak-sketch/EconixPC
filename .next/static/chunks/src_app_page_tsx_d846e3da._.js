425602b0c2255f4d1f11c2c539f9deba3b9d9d91
sayak-sketch <sree.sayakmukherjee@gmail.com>
2026-04-19 16:16:23 +0530

    Cleanup: remove .next directory from tracking

--- .next/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_b9f8ed38._.js",
-  "static/chunks/node_modules_8b4ef6d0._.js"
-],
-    source: "dynamic"
-});
